a935dac0e0937a7af2969170762f68e3dd3f0225
sola <2451477516@qq.com>
2024-10-11 20:30:10 +0800

    开发环境执行脚本

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { init } from './src/utils/format'
 
-init()
+console.log(process.env)
+//开发环境执行
+if (process.env.NODE_ENV === 'development') {
+  init()
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
