aab4b502cd710eae80f2879638150532c5cc23a1
sola <2451477516@qq.com>
2024-10-12 01:00:23 +0800

    添加Algolia搜索

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,26 @@
             <img src="../assets/img/logo.png" alt="logo" />
             <h2>Sola</h2>
         </span>
+        <div id="docsearch"></div>
     </header>
 </template>
 
 <script setup lang="ts">
 import router from '@/router'
+import docsearch from '@docsearch/js';
 
+import '@docsearch/css';
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    docsearch({
+        appId: "D0ABS6ZBZS",
+        apiKey: "f4029465497d63e93ad166438207c696",
+        indexName: "metasola",
+        container: "#docsearch",
+
+    });
+})
 </script>
 
 <style scoped>
@@ -17,6 +31,13 @@ header {
     height: 30px;
     padding: 10px 20px;
     background: linear-gradient(135deg, #fff886, #f072b6);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    position: sticky;
+    top: 0;
+    z-index: 999;
 }
 
 .logo {
